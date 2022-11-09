c538464936e92b66d4d826b886fbcb674bf7f5c8
Tomasz Skolimo <skolim10@gmail.com>
2022-11-09 08:43:43 +0100

    starting settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+import PropTypes from 'prop-types';
+
+export const App = ({ children }) => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {children}
     </div>
   );
+};
+
+App.propTypes = {
+  children: PropTypes.array.isRequired,
 };
